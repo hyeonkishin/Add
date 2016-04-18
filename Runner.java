08c9cff8e00123025a3bff7395afdec844f047a6
Mikita Isakau <harley26.gold@gmail.com>
2016-04-18 10:08:17 +0300

    add new function

--- Runner.java
+++ Runner.java
@@ -2,5 +2,10 @@ public class Runner {
 	public static int STEP = 1;
 
 	public static void main(String[] args){
+         for (int i = 2; i <= 10; i++){
+             System.out.println("Step: " + i);
+         }
+
+
 	}
 }
