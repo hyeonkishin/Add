9c0fae483522e73edb18e044d97cecd8264a08be
eunseokchoi <cesboy@naver.com>
2016-07-29 09:54:39 +0900

    Update Runner.java
    확인하세요~

--- Runner.java
+++ Runner.java
@@ -1,10 +1,14 @@
 public class Runner {
-	public static int STEP = 1;
+	public static int STEP = 10;
 
 	public static void main(String[] args){
-         for (int i = 2; i <= 10; i++){
+         for (int i = 2; i <= 5; i++){
              System.out.println("Step: " + i);
 			 System.out.println("My name is CAPaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+			 System.out.println("My name is CAPaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+			 System.out.println("My name is CAPaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+			 System.out.println("My name is CAPaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+			 
          }
 
 
